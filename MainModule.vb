94f8b87d69477d09f6eca68d2deaa72d7bb2416d
steambird1 <steambird@163.com>
2022-08-10 09:26:44 +0800

    Add GET interpretion

---
+++ MainModule.vb
@@ -0,0 +1,50 @@
+﻿Imports System.Net.Sockets
+Imports System.Threading
+Imports System.IO
+Imports System.Net
+
+Module MainModule
+
+    Public local As IPAddress = IPAddress.Parse("127.0.0.1")
+    Public listener As TcpListener
+    Public islisten As Boolean = True
+
+    Public Structure ReadWrites
+        Public Reader As StreamReader
+        Public Writer As StreamWriter
+        Public Client As TcpClient
+
+        Public Sub New(Reader As StreamReader, Writer As StreamWriter, Client As TcpClient)
+            Me.Reader = Reader
+            Me.Writer = Writer
+            Me.Client = Client
+        End Sub
+    End Structure
+
+    Sub RequestProcessor(Parameter As Object)
+        Dim MyRW As ReadWrites = Parameter
+        ' Process here...
+        Console.Out.Write(MyRW.Reader.ReadToEnd())
+        Console.Out.WriteLine()
+        ' End
+        MyRW.Client.Close()
+    End Sub
+
+    Sub Main()
+        listener = New TcpListener(local, 80)
+        listener.Start()
+        Do
+            Dim stream As NetworkStream
+            Dim sread As StreamReader
+            Dim swrite As StreamWriter
+            Dim current As TcpClient
+            Dim thr As Thread = New Thread(New ParameterizedThreadStart(AddressOf RequestProcessor))
+            current = listener.AcceptTcpClient()
+            stream = current.GetStream()
+            sread = New StreamReader(Stream)
+            swrite = New StreamWriter(Stream)
+            thr.Start(New ReadWrites(sread, swrite, current))
+        Loop
+    End Sub
+
+End Module
---
+++ WebInterpreter.vb
@@ -0,0 +1,38 @@
+﻿' Load WEB Data.
+
+Module WebInterpreter
+
+    Public Class BadWebFormatException
+        Inherits Exception
+        Public Overrides ReadOnly Property Message As String = "Incorrect Web Request Format!"
+    End Class
+
+    Public Class WebInfo
+        Public Property Method As String
+        Public Property Path As String
+        Public Property HTTPVersion As String
+        Public Property Settings As Dictionary(Of String, String)
+
+        ' Only for GET method now.
+        Public Sub New(RequestData As String)
+            Dim spl As String() = Split(RequestData, vbLf)
+            Dim firstline As String = spl(0).Replace(vbCr, "")
+            Dim farg As String() = Split(firstline)
+            If farg.Count() < 3 Then
+                Throw New BadWebFormatException()
+            End If
+            Me.Method = farg(0)
+            Me.Path = farg(1)
+            Me.HTTPVersion = farg(2)
+            For i = 1 To spl.Count - 1
+                Dim myline As String = spl(i).Replace(vbCr, "")
+                Dim argspl As String() = Split(myline, ":", 2)
+                If argspl.Count() < 2 Then
+                    Continue For
+                End If
+                argspl(1) = Trim(argspl(1))
+                Me.Settings(argspl(0)) = argspl(1)
+            Next
+        End Sub
+    End Class
+End Module
