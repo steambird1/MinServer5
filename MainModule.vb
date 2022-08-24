eb087a386854b6bd59026bc968eb84296858cc1d
steambird1 <steambird@163.com>
2022-08-24 13:55:10 +0800

    HTTP GET support BlueBetter bug fixes (See BlueBetter project)

--- MainModule.vb
+++ MainModule.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Net.Sockets
+Imports System.Text.Encoding
 Imports System.Threading
 Imports System.IO
 Imports System.Net
@@ -9,13 +10,29 @@ Module MainModule
     Public local As IPAddress = IPAddress.Parse("127.0.0.1")
     Public listener As TcpListener
     Public islisten As Boolean = True
+    Private isbusy As Integer = 0
+    Public indexname As List(Of String) = New List(Of String)({"", "index.html", "index.htm", "index.blue"})
+    Public contents As Dictionary(Of String, String) = New Dictionary(Of String, String)
+    Public Const interpreter As String = ".blue"
+    'Private lock As Threading.SpinLock = New SpinLock()
+
+    Private Sub InitalizeContents()
+        contents.Add(".html", "text/html")
+        contents.Add(".blue", "text/html")
+        contents.Add(".txt", "text/plain")
+        contents.Add(".jpg", "image/jpeg")
+    End Sub
+
+    Private Sub Initalize()
+        InitalizeContents()
+    End Sub
 
     Public Structure ReadWrites
-        Public Reader As StreamReader
-        Public Writer As StreamWriter
+        Public Reader As BinaryReader
+        Public Writer As BinaryWriter
         Public Client As TcpClient
 
-        Public Sub New(Reader As StreamReader, Writer As StreamWriter, Client As TcpClient)
+        Public Sub New(Reader As BinaryReader, Writer As BinaryWriter, Client As TcpClient)
             Me.Reader = Reader
             Me.Writer = Writer
             Me.Client = Client
@@ -41,6 +58,7 @@ Module MainModule
         Dim s As StringBuilder = New StringBuilder(data)
         s.Replace(vbCr, "")
         s.Replace(vbLf, "\n")
+        s.Replace("\", "\\")
         s.Replace("""", "\""")
         ' & """"
         s.Insert(0, """")
@@ -50,83 +68,197 @@ Module MainModule
 
     ' Note: data is fixed !!!
     Public ReadOnly Property InternalServerError As String = "HTTP/1.1 500 Internal Server Error" & vbCrLf & "Connection: Close" & vbCrLf & "Content-Type: text/html" & vbCrLf & "Content-Length: 211" & vbCrLf & vbCrLf & "<html><head><title>500 - Internal Server Error</title></head><body><h1>500 Internal Server Error</h1><br /><p>The BlueBetter Program didn't provide any content to send.</p><hr /><p>MinServer 5</p></body></html>"
+    Public ReadOnly Property NotFoundError As String = "HTTP/1.1 404 Not Found" & vbCrLf & "Connection: Close" & vbCrLf & "Content-Type: text/html" & vbCrLf & "Content-Length: 159" & vbCrLf & vbCrLf & "<html><head><title>404 - Not Found</title></head><body><h1>404 Not Found</h1><br /><p>Specified path does not exist.</p><hr /><p>MinServer 5</p></body></html>"
 
-    Sub RequestProcessor(Parameter As Object)
+    Sub AsyncRequestProcessor(Parameter As IAsyncResult)
+        isbusy = True
+        Dim tcp As TcpListener = CType(Parameter.AsyncState, TcpListener)
+        Dim current As TcpClient = tcp.EndAcceptTcpClient(Parameter)
+        Dim stream As IO.Stream
+        Dim sread As BinaryReader
+        Dim swrite As BinaryWriter
+        stream = current.GetStream()
+        stream.ReadTimeout = 10000
+        sread = New BinaryReader(stream)
+        swrite = New BinaryWriter(stream)
+        Try
+            RequestProcessor(New ReadWrites(sread, swrite, current))
+            stream.Close()
+        Catch ex As IO.IOException
+            Console.Out.Write("Server abort!")
+            Console.Out.WriteLine()
+        End Try
+    End Sub
+
+    Public Function GetExtension(ByVal MyScript As String)
+        Return MyScript.Substring(MyScript.LastIndexOf("."c))
+    End Function
+
+    Public Function GetDocumentKind(Extension As String)
+        If Not contents.ContainsKey(Extension) Then
+            Return "text/plain"
+        Else
+            Return contents(Extension)
+        End If
+    End Function
+
+    Sub RequestProcessor(Parameter As ReadWrites)
+        Dim ExceptionOccured As Boolean = False
         Dim MyRW As ReadWrites = Parameter
+        Dim MySenderData As New WebString
         ' Process here...
         Dim MyWebInfo As WebInfo = New WebInfo(MyRW.Reader)
+        If Trim(MyWebInfo.Path) = "" Then
+            MyRW.Client.Close()
+            Exit Sub
+        End If
+        If MyWebInfo.Path(0) = "/" Or MyWebInfo.Path(0) = "\" Then
+            MyWebInfo.Path = MyWebInfo.Path.Remove(0, 1)
+        End If
         ' Create BlueBetter Environment under temp directory
         Dim t_add As String = Environ("temp") & "\"
-        Dim DirectoryToBluebetter As String = GenerateRandom(t_add)
-        For Each i In BlueESS
-            FileCopy(pather & "\" & i, DirectoryToBluebetter)
+        Dim MyScriptRaw As String = pather & "\" & MyWebInfo.Path
+        Dim MyScript As String = MyScriptRaw
+        ' Detect if directory
+        For Each i In indexname
+            MyScript = MyScriptRaw & i
+            If My.Computer.FileSystem.FileExists(MyScript) Then
+                Exit For
+            End If
         Next
-        ' Requires ANSI for BlueBetter 4
-        Dim ActiveExecuter As String = DirectoryToBluebetter & "\execute.blue"
-        Dim ActiveScript As StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(ActiveExecuter, False, Text.Encoding.Default)
-        Dim MyHeader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(pather & "\WebHeader.blue")
-        Dim MyScript As String = pather & "\" & MyWebInfo.Path
-        ActiveScript.Write(MyHeader.ReadToEnd())
-        MyHeader.Close()
-        ActiveScript.WriteLine()
-        ActiveScript.Write("set receiver.path=" & SetQuotes(MyWebInfo.Path))
-        ActiveScript.WriteLine()
-        ActiveScript.Write("set receiver.http_version=" & SetQuotes(MyWebInfo.HTTPVersion))
-        ActiveScript.WriteLine()
-        ActiveScript.Write("set receiver.method=" & SetQuotes(MyWebInfo.Method))
-        ActiveScript.WriteLine()
-        For Each i In MyWebInfo.Settings
-            ActiveScript.Write("set receiver.attributes#" & SetQuotes(i.Key) & "=" & SetQuotes(i.Value))
+        If Not My.Computer.FileSystem.FileExists(MyScript) Then
+            Console.Out.Write("404: " & MyScript)
+            MySenderData = New WebString(NotFoundError)
+            GoTo BeginWriting
+        End If
+        Dim MyExtension As String = GetExtension(MyScript)
+        Dim DocKind As String = GetDocumentKind(MyExtension)
+
+        If MyExtension = interpreter Then
+            ' ... Execute BlueBetter ...
+            Dim DirectoryToBluebetter As String = GenerateRandom(t_add)
+            Console.Out.Write("Directory: " & DirectoryToBluebetter)
+            Console.Out.WriteLine()
+            My.Computer.FileSystem.CreateDirectory(DirectoryToBluebetter)
+            For Each i In BlueESS
+                FileCopy(pather & "\" & i, DirectoryToBluebetter & "\" & i)
+            Next
+            ' Requires ANSI for BlueBetter 4
+            Dim ActiveExecuter As String = DirectoryToBluebetter & "\execute.blue"
+            Dim ActiveScript As StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(ActiveExecuter, False, Text.Encoding.Default)
+            Dim MyHeader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(pather & "\WebHeader.blue")
+            ActiveScript.Write(MyHeader.ReadToEnd())
+            MyHeader.Close()
             ActiveScript.WriteLine()
-        Next
-        ' Get content data...
-        Dim MyContent As WebInfo.PostInfo = MyWebInfo.PostData
-        Dim MyData = MyContent.Data
-        Dim DataCounter As Integer = 0
-        For Each i In MyData
-            Dim TempName As String = "__postdata_" & DataCounter
-            ActiveScript.Write("set " & TempName & "=new post_data")
+            ActiveScript.Write("set receiver.path=" & SetQuotes(MyWebInfo.Path))
             ActiveScript.WriteLine()
-            ' Get file for it
-            Dim CurrentFilename As String = GenerateRandom(DirectoryToBluebetter)
-            Dim CurrentStream As BinaryWriter = New BinaryWriter(File.Open(CurrentFilename, FileMode.Create))
-            i.SaveTo(CurrentStream)
-            CurrentStream.Close()
-            ActiveScript.Write("set " & TempName & ".myfile=" & SetQuotes(CurrentFilename))
+            ActiveScript.Write("set receiver.http_version=" & SetQuotes(MyWebInfo.HTTPVersion))
             ActiveScript.WriteLine()
-            For Each j In i.Settings
-                ActiveScript.Write("set " & TempName & ".attributes#" & SetQuotes(j.Key) & "=" & SetQuotes(j.Value))
+            ActiveScript.Write("set receiver.method=" & SetQuotes(MyWebInfo.Method))
+            ActiveScript.WriteLine()
+            For Each i In MyWebInfo.Settings
+                ActiveScript.Write("set receiver.attributes#" & SetQuotes(i.Key) & "=" & SetQuotes(i.Value))
                 ActiveScript.WriteLine()
             Next
-            DataCounter += 1
-        Next
-        Dim MySender As String = GenerateRandom(DirectoryToBluebetter)
-        ActiveScript.Write("run sender._setfiles " & SetQuotes(MySender))
-        ActiveScript.WriteLine()
-        ActiveScript.WriteLine()
-        ' Read file to run
-        Dim MyCodeContent As StreamReader = My.Computer.FileSystem.OpenTextFileReader(MyScript)
-        ActiveScript.Write(MyCodeContent.ReadToEnd())
-        MyCodeContent.Close()
-        ActiveScript.Close()
-        ' Execute!
-        Shell(ActiveExecuter, AppWinStyle.Hide, True)
-        ' Write response
-        Try
-            Dim MySenderStream As BinaryReader = New BinaryReader(File.Open(MySender, FileMode.Open))
-
-        Catch ex As FileNotFoundException
-            Console.Out.Write("The program doesn't return anything to return!")
+            ' Get content data...
+            Dim MyContent As WebInfo.PostInfo = MyWebInfo.PostData
+            Dim MyData = MyContent.Data
+            Dim DataCounter As Integer = 0
+            For Each i In MyData
+                Dim TempName As String = "__postdata_" & DataCounter
+                ActiveScript.Write("set " & TempName & "=new post_data")
+                ActiveScript.WriteLine()
+                ' Get file for it
+                Dim CurrentFilename As String = GenerateRandom(DirectoryToBluebetter)
+                Dim CurrentStream As BinaryWriter = New BinaryWriter(File.Open(CurrentFilename, FileMode.Create), Encoding.Default)
+                i.SaveTo(CurrentStream)
+                CurrentStream.Close()
+                ActiveScript.Write("set " & TempName & ".myfile=" & SetQuotes(CurrentFilename))
+                ActiveScript.WriteLine()
+                For Each j In i.Settings
+                    ActiveScript.Write("set " & TempName & ".attributes#" & SetQuotes(j.Key) & "=" & SetQuotes(j.Value))
+                    ActiveScript.WriteLine()
+                Next
+                DataCounter += 1
+            Next
+            Dim MySender As String = GenerateRandom(DirectoryToBluebetter)
+            ActiveScript.Write("run sender._setfiles " & SetQuotes(MySender))
+            ActiveScript.WriteLine()
+            ActiveScript.Write("# Normal code -----")
+            ActiveScript.WriteLine()
+            ActiveScript.WriteLine()
+            ' Read file to run
+            Dim MyCodeContent As StreamReader = Nothing
+            Try
+                MyCodeContent = My.Computer.FileSystem.OpenTextFileReader(MyScript)
+            Catch ex As FileNotFoundException
+                Console.Out.Write("404: " & MyScript)
+                MySenderData = NotFoundError
+                GoTo BeginWriting
+            End Try
+            ActiveScript.Write(MyCodeContent.ReadToEnd())
+            MyCodeContent.Close()
+            ActiveScript.Close()
+            Console.Out.Write("End of script loader ...")
             Console.Out.WriteLine()
-            MyRW.Writer.Write(InternalServerError)
-            MyRW.Writer.WriteLine()
-        End Try
-        ' End
-        MyRW.Client.Close()
+            ' Execute!
+            Shell(DirectoryToBluebetter & "\BlueBetter4.exe " & ActiveExecuter & "", AppWinStyle.Hide, True)
+            ' Write response
+NoExecuted:
+            Try
+                Dim MySenderStream As StreamReader = My.Computer.FileSystem.OpenTextFileReader(MySender, [Default])
+                MySenderData.Append(MySenderStream.ReadToEnd())
+                MySenderStream.Close()
+            Catch ex As FileNotFoundException
+                Console.Out.Write("500: The program doesn't return anything to return!")
+                Console.Out.WriteLine()
+                MyRW.Writer.Write(InternalServerError)
+                MyRW.Writer.Write(vbLf)
+                ExceptionOccured = True
+            End Try
+            My.Computer.FileSystem.DeleteDirectory(DirectoryToBluebetter, FileIO.DeleteDirectoryOption.DeleteAllContents)
+            My.Computer.FileSystem.DeleteFile(MySender)
+        Else
+            ' Send the whole file
+            'Dim WholeReader As StreamReader = New StreamReader(File.OpenRead(MyScript))
+            'Dim WRead As String = WholeReader.ReadToEnd()
+            ' To modify encoding and apply to everywhere!
+            Dim WholeReader As BinaryReader = New BinaryReader(File.Open(MyScript, FileMode.Open))
+            Dim WLength As Long = WholeReader.BaseStream.Length
+            'Dim WholeData As Char() = WholeReader.ReadChars(WLength)
+            Dim WholeData As Byte() = WholeReader.ReadBytes(WLength)
+            Console.Out.Write("Length of normal stream: " & WLength)
+            Console.Out.WriteLine()
+            MySenderData.Append("HTTP/1.1 200 OK")
+            MySenderData.AppendLine()
+            MySenderData.Append("Content-Type: " & DocKind)
+            MySenderData.AppendLine()
+            MySenderData.Append("Content-Length: " & WLength)
+            MySenderData.AppendLine()
+            MySenderData.AppendLine()
+            MySenderData.Append(WholeData)
+            ' Promisor
+            Dim Promisor As BinaryWriter = New BinaryWriter(File.Open(pather & "\Promise.bin", FileMode.Create), Encoding.Default)
+            Promisor.Write(MySenderData.ByteData)
+            Promisor.Close()
+            ' End of promisor test
+            WholeReader.Close()
+        End If
+
+BeginWriting: If Not ExceptionOccured Then
+            MyRW.Writer.Write(MySenderData.ByteData)
+            MyRW.Writer.Flush()
+        End If
+        MyRW.Reader.Close()
+        MyRW.Writer.Close()
+FinishWriting: MyRW.Client.Close()
+        Console.Out.Write("End of sender ...")
+        Console.Out.WriteLine()
     End Sub
 
 
     Sub Main()
+        Initalize()
 
         For Each i In FileToCheck
             If Not My.Computer.FileSystem.FileExists(pather & "\" & i) Then
@@ -144,17 +276,35 @@ Module MainModule
 
         listener = New TcpListener(local, 80)
         listener.Start()
+        Dim currentBus As Integer = 0
+        Dim output As Boolean = False
+        isbusy = False
         Do
-            Dim stream As NetworkStream
-            Dim sread As StreamReader
-            Dim swrite As StreamWriter
-            Dim current As TcpClient
+            'Dim stream As NetworkStream
+            'Dim sread As StreamReader
+            'Dim swrite As StreamWriter
+            'Dim current As TcpClient
             Dim thr As Thread = New Thread(New ParameterizedThreadStart(AddressOf RequestProcessor))
-            current = listener.AcceptTcpClient()
-            stream = current.GetStream()
-            sread = New StreamReader(stream)
-            swrite = New StreamWriter(stream)
-            thr.Start(New ReadWrites(sread, swrite, current))
+            Dim taken As Boolean = False
+            Dim currentAsync As IAsyncResult = listener.BeginAcceptTcpClient(New AsyncCallback(AddressOf AsyncRequestProcessor), listener)
+            ' Wait ...
+            Do Until isbusy
+                If Not output Then
+                    Console.Out.Write("Waiting ...")
+                    Console.Out.WriteLine()
+                    output = True
+                End If
+            Loop
+            isbusy = False
+            output = False
+            'stream = current.GetStream()
+            'stream.ReadTimeout = 10000
+            'stream.WriteTimeout = 10000
+            'sread = New StreamReader(stream)
+            'swrite = New StreamWriter(stream)
+            'thr.Name = "HTTP Thread"
+            'thr.Start(New ReadWrites(sread, swrite, current))
+            'stream.Close()  '???
         Loop
     End Sub
 
--- WebInterpreter.vb
+++ WebInterpreter.vb
@@ -9,12 +9,137 @@ Module WebInterpreter
         Public Overrides ReadOnly Property Message As String = "Incorrect Web Request Format!"
     End Class
 
+    Public Class NoByteStringException
+        Inherits Exception
+
+        Public Sub New(message As String)
+            MyBase.New(message)
+        End Sub
+    End Class
+
+    ' Must use ToStringWithEncoding() when regard it as string.
+    <Serializable>
+    Public Class WebString
+
+        Private _byteData As Byte() = {}
+        'Private Referencer As StringBuilder = New StringBuilder
+        Private ReferenceLine As Byte
+
+        Public ReadOnly Property Length As Long
+            Get
+                Return _byteData.Count()
+            End Get
+        End Property
+
+        Public Property ByteData As Byte()
+            Get
+                Return _byteData
+            End Get
+            Private Set(value As Byte())
+                _byteData = value
+            End Set
+        End Property
+
+        Public Property Capacity As Long
+            Get
+                Return _byteData.Count()
+            End Get
+            Set(value As Long)
+                ReDim Preserve _byteData(value)
+            End Set
+        End Property
+
+        Public Sub New()
+            ReferenceLine = AscW(vbLf) 'Referencer.AppendLine().ToString()(0)
+            'ReDim Me._byteData(0)
+        End Sub
+
+        Public Sub New(Str As String)
+            Me.Append(Str.ToCharArray())
+        End Sub
+
+        Public Sub New(Str As WebString)
+            Me._byteData = Str._byteData
+        End Sub
+
+        Public Sub New(Str As Byte())
+            Me._byteData = Str
+        End Sub
+
+        Public Sub New(str As Char())
+            Me.Append(str)
+        End Sub
+
+        Public Sub New(caps As Long)
+            Me.Capacity = caps
+        End Sub
+
+        Public Sub Append(Str As WebString)
+            Dim Origin As Long = Me._byteData.LongCount()
+            ReDim Preserve Me._byteData(Origin + Str._byteData.LongCount() - 1)
+            Array.Copy(Str._byteData, 0, Me._byteData, Origin, Str._byteData.Count())
+        End Sub
+
+        Public Sub Append(Str As Char(), Optional ByVal Encoding As Encoding = Nothing)
+            If IsNothing(Encoding) Then
+                Encoding = Encoding.Default
+            End If
+            Me.Append(New WebString(Encoding.GetBytes(Str)))
+        End Sub
+
+        Public Sub Append(Str As Byte())
+            Me.Append(New WebString(Str))
+        End Sub
+
+        Public Shared Widening Operator CType(ByVal Str As String) As WebString
+            Return New WebString(Str)
+        End Operator
+
+        Public Shared Narrowing Operator CType(ByVal Str As WebString) As String
+            Throw New NoByteStringException("Please use ToStringWithEncoding() instead !")
+        End Operator
+
+        Public Sub AppendLine()
+            Me.Append({ReferenceLine})
+        End Sub
+
+        Public Shared Operator +(ByVal Str1 As WebString, ByVal Str2 As WebString) As WebString
+            Dim Str As WebString = Str1
+            Str.Append(Str2)
+            Return Str
+        End Operator
+
+        Public Function ToStringWithEncoding(Optional ByVal Encoding As Encoding = Nothing) As String
+            If IsNothing(Encoding) Then
+                Encoding = Encoding.Default
+            End If
+            Return Encoding.GetChars(Me._byteData)
+        End Function
+
+        ' Please modify everywhere.
+
+        Public Overrides Function ToString() As String
+            Throw New NoByteStringException("Please use ToStringWithEncoding() instead !")
+        End Function
+
+    End Class
+
+    Public Function ReadBinaryLine(ByRef Reader As IO.BinaryReader) As WebString
+        Dim ws As WebString = New WebString
+        Dim ch As Byte
+        Do Until ch = AscW(vbLf)
+            ch = Reader.ReadByte()
+            ws.Append({ch})
+        Loop
+        Return ws
+    End Function
+
     Public Class WebInfo
         Public Property Method As String = ""
         Public Property Path As String = ""
         Public Property HTTPVersion As String = ""
         Public Property Settings As Dictionary(Of String, String) = New Dictionary(Of String, String)
-        Public Property Content As String = ""
+        Public Property Content As WebString = New WebString()
 
         Public ReadOnly Property MyBoundary As String
             Get
@@ -34,15 +159,16 @@ Module WebInterpreter
 
             Public Structure SingleData
                 Public Property Settings As Dictionary(Of String, String)
-                Public Property Content As String
+                Public Property Content As WebString
 
-                Public Sub New(Settings As Dictionary(Of String, String), Content As String)
+                Public Sub New(Settings As Dictionary(Of String, String), Content As WebString)
                     Me.Settings = Settings
                     Me.Content = Content
                 End Sub
 
                 Public Sub SaveTo(Stream As IO.BinaryWriter)
-                    Stream.Write(Me.Content)
+                    'Stream.Write(Me.Content)
+                    Stream.Write(Me.Content.ByteData)
                 End Sub
 
             End Structure
@@ -57,15 +183,15 @@ Module WebInterpreter
                 Dim bd As String = Me.MyBoundary
                 Dim temp As PostInfo = New PostInfo
                 temp.Data = New List(Of PostInfo.SingleData)
-                Dim spl As String() = Split(Me.Content, vbLf)
+                Dim spl As String() = Split(Me.Content.ToStringWithEncoding(), vbLf)
                 Dim current As PostInfo.SingleData = Nothing
                 Dim content As Boolean = False
-                Dim mycontent As StringBuilder = New StringBuilder
+                Dim mycontent As New WebString
                 For Each i In spl
                     If i.IndexOf(bd) > 0 Then
                         If Not IsNothing(current) Then
                             current.Content = mycontent.ToString()
-                            mycontent.Clear()
+                            mycontent = New WebString
                         End If
                         If i(i.Length - 1) = "-" Then
                             Exit For
@@ -91,15 +217,16 @@ Module WebInterpreter
         End Property
 
         ' Only for GET method now.
-        Public Sub New(RequestData As String)
-            If Trim(RequestData).Length <= 0 Then
+
+        Protected Sub GenerateFrom(RequestData As WebString)
+            If Trim(RequestData.ToStringWithEncoding()).Length <= 0 Then
                 Exit Sub
             End If
             Dim i As Integer
-            Dim spl As String() = Split(RequestData, vbLf)
+            Dim spl As String() = Split(RequestData.ToStringWithEncoding(), vbLf)
             Dim firstline As String = spl(0).Replace(vbCr, "")
             Dim farg As String() = Split(firstline)
-            Dim mycontent As StringBuilder
+            Dim mycontent As WebString = Nothing
             If farg.Count() < 3 Then
                 Throw New BadWebFormatException()
             End If
@@ -120,19 +247,75 @@ Module WebInterpreter
                     If Me.Settings.ContainsKey("Content-Length") Then
                         slen = Val(Me.Settings("Content-Length"))
                     End If
-                    mycontent = New StringBuilder(slen)
+                    mycontent = New WebString(slen)
                 End If
                 Dim argspl As String() = Split(myline, ":", 2)
                 If argspl.Count() < 2 Then
                     Continue For
                 End If
                 argspl(1) = Trim(argspl(1))
-                Me.Settings(argspl(0)) = argspl(1)  '....!
+                Me.Settings(argspl(0)) = argspl(1)
             Next
+            Console.Out.Write("End of analyze...")
+            Console.Out.WriteLine()
         End Sub
 
-        Public Sub New(RequestStream As IO.StreamReader)
-            Me.New(RequestStream.ReadToEnd())
+        Public Sub New(RequestData As String)
+            GenerateFrom(RequestData)
+        End Sub
+
+        Public EndOfLines As List(Of Char) = New List(Of Char)({vbCr, vbLf})
+
+        ' To test if POST.
+        Public Sub New(RequestStream As IO.BinaryReader)
+            ' Test
+            Console.Out.Write("WebInfo.New (IO.StreamReader)")
+            Console.Out.WriteLine()
+            ' End
+            Try
+                Dim tmp As WebString = New WebString
+                Dim myLength As Long = 0
+                ' Read first line for 'tmp'.
+                Dim CurrentData As Byte()
+                Do
+                    Try
+                        Dim CurrentLine As String = ReadBinaryLine(RequestStream).ToStringWithEncoding()
+                        Console.Out.Write(CurrentLine) '  Console.Out.WriteLine()
+                        tmp.Append(CurrentLine)
+                        Dim LastChar, FirstChar As Char
+                        If CurrentLine.Length > 0 Then
+                            LastChar = CurrentLine(CurrentLine.Length - 1)
+                            FirstChar = CurrentLine(0)
+                        Else
+                            LastChar = vbLf
+                            FirstChar = vbLf
+                        End If
+
+                        If EndOfLines.Contains(FirstChar) And EndOfLines.Contains(LastChar) Then
+                            ' An empty line!
+                            ' Content must be began
+                            If myLength > 0 Then
+                                ReDim CurrentData(myLength + 1)
+                                CurrentData = RequestStream.ReadBytes(myLength)
+                                tmp.Append(CurrentData)
+                            End If
+                            Exit Do
+                        Else
+                            ' Must be attribute or beginning.
+                            Dim spl As String() = Split(CurrentLine, " ", 2)
+                            If spl.Count() >= 2 And spl(0) = "Content-Length:" Then
+                                myLength = Val(spl(1))
+                            End If
+                        End If
+                    Catch ex As IO.EndOfStreamException
+                        Exit Do
+                    End Try
+                Loop
+                GenerateFrom(tmp)
+            Catch ex As IO.IOException
+                Console.Out.Write("Timeout!")
+                Console.Out.WriteLine()
+            End Try
         End Sub
 
         Public Sub DebugOutput()
