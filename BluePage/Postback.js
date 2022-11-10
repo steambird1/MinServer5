e9a051031e0795792f5e8735ed492ec60e13d047
steambird1 <steambird@163.com>
2022-11-10 22:00:33 +0800

    PostBack bug fixes

--- BluePage/Postback.js
+++ BluePage/Postback.js
@@ -1,8 +1,12 @@
 function mins_dealing(content) {
-    var spls = content.split("\n");
-    for (let i = 0; i < spls.length; i++) {
-        var curs = spls[i].split("=", 2);
-        var left_name = curs[0].split(".", 2);
-        document.getElementById(left_name[0])[left_name[1]] = curs[1];
+    try {
+        var spls = content.split("\n");
+        for (let i = 0; i < spls.length; i++) {
+            var curs = spls[i].split("=", 2);
+            var left_name = curs[0].split(".", 2);
+            document.getElementById(left_name[0])[left_name[1]] = curs[1];
+        }
+    } catch (err) {
+        // TypeError?
     }
 }
